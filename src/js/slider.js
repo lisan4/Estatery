aa7f50654231103c6d77ced5edfa5875cf1f412a
Ivan <mloveannn@mail.ru>
2024-11-11 20:58:01 +0300

    slider and prom

---
+++ src/js/slider.js
@@ -0,0 +1,15 @@
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 32,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+
+   
+});
